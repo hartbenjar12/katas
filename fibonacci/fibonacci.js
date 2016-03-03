bb81bd9cbcf878aa5adf53a3349be8ad69e426d1
Aaron Evans <aaron.eavns@hart.com>
2016-03-03 11:21:12 -0800

    create validator object to externalize input validations (but still check inside the function)

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,8 +1,8 @@
 Array.prototype.add = Array.prototype.push
 
 function fibonacci(iterations = 2) {
-	check_iterations(iterations)
-
+	validate.iterations(iterations)
+	
 	let sequence = [1, 2]
 
 	while(sequence.length < iterations) {
@@ -12,17 +12,13 @@ function fibonacci(iterations = 2) {
 	return sequence
 }
 
-function check_iterations() {
-	if (! Number.isInteger(iterations) || iterations < 2) { 
-		throw Error('fibonacci must be called with a number of iterations (2 or more)')
+
+var validate = {
+	iterations: function(iterations) {
+		if (! Number.isInteger(iterations) || iterations < 2) { 
+			throw Error('fibonacci must be called with a number of iterations (2 or more)')
+		}
 	}
 }
 
 module.exports = fibonacci
-
-// tests
-var fib = fibonacci
-console.log(fib(1))
-console.log(fib(2))
-console.log(fib(3))
-console.log(fib(10))
