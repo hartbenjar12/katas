8161395243e731e0ae8f7e6fe345d971ad72327d
Aaron Evans <aaron.eavns@hart.com>
2016-03-03 11:53:16 -0800

    declare length in loop

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,3 +1,23 @@
+function fibonacci(iterations) {
+	validate.iterations(iterations)
+
+	// the fibonacci sequence starts here
+	let sequence = [1, 2]
+
+	while(let length = sequence.length < iterations) {
+		// the next number in the sequence is the sum of the last two 
+		let next = sequence[length - 1] + sequence[length - 2]
+		sequence.append(next)
+	}
+
+	return sequence
+}
+
+module.exports = fibonacci
+
+
+// helpers that will not be exported
+
 Array.prototype.append = Array.prototype.push
 
 var validate = {
@@ -8,20 +28,3 @@ var validate = {
 	}
 }
 
-
-function fibonacci(iterations) {
-	validate.iterations(iterations)
-
-	// the fibonacci sequence starts here
-	let sequence = [1, 2]
-
-	while(sequence.length < iterations) {
-		// the next number in the sequence is the sum of the last two 
-		let next = sequence[sequence.length - 1] + sequence[sequence.length - 2]
-		sequence.append(next)
-	}
-
-	return sequence
-}
-
-module.exports = fibonacci
