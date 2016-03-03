d52f26357fb6613b3b34b73b634e749ee7a112fa
Aaron Evans <aaron.eavns@hart.com>
2016-03-03 12:08:36 -0800

    ok, back to the simpler, clearer, and more efficient length based calculation for next value
    This really shows that tricky "cool" constructs like reduce are usually not the best way to do things

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -6,9 +6,9 @@ function fibonacci(iterations) {
 	// the fibonacci sequence starts here
 	let sequence = [1, 2]
 
-	while(sequence.length < iterations) {
+	while(let length = sequence.length < iterations) {
 		// the next number in the sequence is the sum of the last two 
-		let next = sequence.slice(-2).reduce( (y,z)=> { return y+z })
+		let next = sequence[length - 1] + sequence[length - 2]
 		sequence.append(next)
 	}
 
