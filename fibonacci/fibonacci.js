14276eb053d4dba77e0c7261806878e030eea23e
Aaron Evans <aaron.eavns@hart.com>
2016-03-03 11:54:35 -0800

    make length a const

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -4,7 +4,7 @@ function fibonacci(iterations) {
 	// the fibonacci sequence starts here
 	let sequence = [1, 2]
 
-	while(let length = sequence.length < iterations) {
+	while(const length = sequence.length < iterations) {
 		// the next number in the sequence is the sum of the last two 
 		let next = sequence[length - 1] + sequence[length - 2]
 		sequence.append(next)
