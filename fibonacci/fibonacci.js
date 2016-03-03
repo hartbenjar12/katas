b0d410e2fadccdbcd98b4af9a05eff04da5d5a92
Aaron Evans <aaron.eavns@hart.com>
2016-03-03 11:45:27 -0800

    ok, I broke down and added another variable "next" for readability
    I resisted the urge to write: sequence.split(-2).reduce( (y, z)=> { return y+z a))
    But I added an "append" method to Array.prototype because push is less aesthetically pleasing.  (It's just an alias for push)

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,18 +1,19 @@
-Array.prototype.add = Array.prototype.push
+Array.prototype.append = Array.prototype.push
 
-function fibonacci(iterations = 2) {
+function fibonacci(iterations) {
 	validate.iterations(iterations)
 
 	// the fibonacci sequence starts here
 	let sequence = [1, 2]
 
 	while(sequence.length < iterations) {
-		sequence.add() + sequence[sequence.length - 2])		
+		// the next number in the sequence is the sum of the last two 
+		let next = sequence[sequence.length - 1] + sequence[sequence.length - 2]
+		sequence.append(next)
 	}
 
 	return sequence
 }
-
 
 var validate = {
 	iterations: function(iterations) {
