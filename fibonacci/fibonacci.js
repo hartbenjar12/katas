89b92c49174900bac1f37ab6b20fd4baf86a83ab
Aaron Evans <aaron.eavns@hart.com>
2016-03-03 10:49:50 -0800

    I'm adamant that the fibonacci starts at 1. I don't care what mathematicians say.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,5 @@
 function fibonacci(iterations = 2) {
-	let sequence = [0, 1]  // every fibonacci sequence starts here
+	let sequence = [1, 2]  // every fibonacci sequence starts here
 
 	if (! Number.isInteger(iterations) || iterations < 2) { 
 		throw Error('fibonacci must be called with a number of iterations (2 or more)')
